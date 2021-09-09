f5f6480664cdbad4495ad8cda143b81a18e2d4b5
Dave Wright <dave.wright@unicard-uk.com>
2021-09-09 11:54:52 +0100

    Prints next 5 buses to arrive

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
 
+class BusArrival:
+    def __init__(self, busNumber, expectedArrivalTime):
+        self.busNumber = busNumber
+        self.expectedArrivalTime = expectedArrivalTime
+
 def main():
-    r = requests.get('http://www.google.com')
-    print("Welcome to BusBoard.")
+    atcoCode = '490000077E'
+    appId = 'f87f46ad'
+    appKey = '053321f8d9a6af06b9c6227e9446f256'
+    r = requests.get(f'http://transportapi.com/v3/uk/bus/stop/{atcoCode}/live.json?app_id={appId}&app_key={appKey}')
+    response = r.json()
+    departures = response["departures"]
+    busInfo = []
+    for departure in departures:
+        for departureEntry in departures[departure]:
+            busInfo.append(BusArrival(departure, departureEntry["expected"]["arrival"]["time"]))
+
+    busInfo.sort(key=lambda busArrival: busArrival.expectedArrivalTime)
+
+    for bus in busInfo[:5]:
+        print(f'Bus: {bus.busNumber} arriving at {bus.expectedArrivalTime}')
 
 if __name__ == "__main__":
     main()
