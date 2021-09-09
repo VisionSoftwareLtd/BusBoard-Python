1fd1bf5e0c301f475a754cb2d69f3f40e7549f79
Dave Wright <dave.wright@unicard-uk.com>
2021-09-09 11:20:29 +0100

    Removed poetry and used pip instead for the moment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import requests
+
 def main():
+    r = requests.get('http://www.google.com')
     print("Welcome to BusBoard.")
 
-
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
